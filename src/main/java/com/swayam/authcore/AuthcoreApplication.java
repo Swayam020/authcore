3c0588b0d4485250a8b45f8a2de003e2393ddd5e
Swayam020 <swayammaheshwari02@gmail.com>
2026-06-06 10:36:30 +0530

    chore: scaffold Spring Boot 3 project with core dependencies

---
+++ src/main/java/com/swayam/authcore/AuthcoreApplication.java
@@ -0,0 +1,13 @@
+package com.swayam.authcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcoreApplication.class, args);
+	}
+
+}
